c6b60326412f043017683e68259440097ea739b4
Malini-SF4235 <malini.selvarasu@syncfusion.com>
2025-06-20 17:43:27 +0530

    Committed the sample for the KB article - How to show the busy indicator while loading the data in the WPF SfDataGrid?

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # How-to-show-the-busy-indicator-while-loading-the-data-in-the-SfDataGrid
+
+You can show an indication of the data loading with the help of the BusyIndicator while in the WPF DataGrid. You can display the BusyIndicator by setting the BusyIndicator.IsBusy to true when loading the data. Once loaded, you can stop it by setting the BusyIndicator.IsBusy to false by handling the ItemsSourceChanged event of the SfDataGrid.
+
+You can show an indication of the data loading with the help of the BusyIndicator while in the WPF DataGrid. You can display the BusyIndicator by setting the BusyIndicator.IsBusy to true when loading the data. Once loaded, you can stop it by setting the BusyIndicator.IsBusy to false by handling the ItemsSourceChanged event of the SfDataGrid.
+
+```xaml
+<Syncfusion:SfBusyIndicator Name="sfBusyIndicator"
+                        IsBusy=”True”
+                        VerticalAlignment="Center"
+                        AnimationType="Gear">
+</Syncfusion:SfBusyIndicator>
+ ```
+
+Here, the BusyIndicator is stopped after the SfDataGrid.ItemsSource is changed by handling the ItemsSourceChanged event.
+
+**C#**
+ 
+ ```csharp
+SfdataGrid.Loaded += SfdataGrid_Loaded;
+SfdataGrid.ItemsSourceChanged += SfdataGrid_ItemsSourceChanged;
+async void SfdataGrid_Loaded(object sender, RoutedEventArgs e)
+{
+    this.SfdataGrid.ItemsSource = await (this.DataContext as ViewModel).GetRecords();
+}
+void SfdataGrid_ItemsSourceChanged(object sender, GridItemsSourceChangedEventArgs e)
+{
+    sfBusyIndicator.IsBusy = false;
+}
+  ```
+
+The following screenshot shows the BusyIndictor before loading the data into the view.
+
+![alt text](image.png)
---
+++ WPF/Model/Model.cs
@@ -0,0 +1,124 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridDemo
+{
+    public class Model : NotificationObject
+    {
+        private int _employeeId;
+        public int EmployeeId
+        {
+            get
+            {
+                return _employeeId;
+            }
+            set
+            {
+                _employeeId = value;
+                this.RaisePropertyChanged("EmployeeId");
+            }
+        }
+
+        private string _employeeName;
+        public string EmployeeName
+        {
+            get
+            {
+                return _employeeName;
+            }
+            set
+            {
+                _employeeName = value;
+                this.RaisePropertyChanged("EmployeeName");
+            }
+        }
+
+        private string _employeeDesignation;
+        public string EmployeeDesignation
+        {
+            get
+            {
+                return _employeeDesignation;
+            }
+            set
+            {
+                _employeeDesignation = value;
+                this.RaisePropertyChanged("EmployeeDesignation");
+            }
+        }
+
+        private int _employeeAge;
+        public int EmployeeAge
+        {
+            get
+            {
+                return _employeeAge;
+            }
+            set
+            {
+                _employeeAge = value;
+                this.RaisePropertyChanged("EmployeeAge");
+            }
+        }
+
+        private string _employeeArea;
+        public string EmployeeArea
+        {
+            get
+            {
+                return _employeeArea;
+            }
+            set
+            {
+                _employeeArea = value;
+                this.RaisePropertyChanged("EmployeeArea");
+            }
+        }
+
+        private string _employeeGender;
+        public string EmployeeGender
+        {
+            get
+            {
+                return _employeeGender;
+            }
+            set
+            {
+                _employeeGender = value;
+                this.RaisePropertyChanged("EmployeeGender");
+            }
+        }
+
+        private int _experienceInMonth;
+        public int ExperienceInMonth
+        {
+            get
+            {
+                return _experienceInMonth;
+            }
+            set
+            {
+                _experienceInMonth = value;
+                this.RaisePropertyChanged("ExperienceInMonth");
+            }
+        }
+
+        private double _employeeSalary;
+        public double EmployeeSalary
+        {
+            get
+            {
+                return _employeeSalary;
+            }
+            set
+            {
+                _employeeSalary = value;
+                this.RaisePropertyChanged("EmployeeSalary");
+            }
+        }
+    }
+}
